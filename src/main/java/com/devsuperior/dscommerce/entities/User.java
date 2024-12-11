cb96fa9b5fb76b9ef3d9ddd769262f90df9fdd91
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2024-12-10 23:40:29 -0300

    Domain mode User Role, seed

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -3,9 +3,7 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -24,6 +22,13 @@ public class User {
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User() {
     }
@@ -87,6 +92,26 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    //foi adicionado somente o método GET para essa coleção
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    //metodo adicional
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    //metodo adicional
+    public boolean hasRole(String roleName) {
+        for(Role r : roles) {
+            if(r.getAuthority().equals(roleName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
