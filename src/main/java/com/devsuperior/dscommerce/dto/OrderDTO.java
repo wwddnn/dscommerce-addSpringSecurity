d5b009219869a4f550431c2253ccdc8b0ff93b47
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2024-12-14 23:30:04 -0300

    Order DTO, get order by id

---
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
@@ -0,0 +1,77 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.entities.OrderItem;
+import com.devsuperior.dscommerce.entities.OrderStatus;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDTO {
+
+    private Long id;
+    private Instant moment;
+    private OrderStatus status;
+
+    //É OUTRO DTO
+    private ClientDTO client;
+
+    //É OUTRO DTO
+    private PaymentDTO payment;
+
+    //É OUTRO DTO
+    private List<OrderItemDTO> items = new ArrayList<>();
+
+    public OrderDTO(Long id, Instant moment, OrderStatus status, ClientDTO client, PaymentDTO payment) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+        this.payment = payment;
+    }
+
+    public OrderDTO(Order entity) {
+        id = entity.getId();
+        moment = entity.getMoment();
+        status = entity.getStatus();
+        client = new ClientDTO(entity.getClient());
+        payment = (entity.getPayment() == null) ? null : new PaymentDTO(entity.getPayment());
+        for (OrderItem item : entity.getItems()) {
+            OrderItemDTO itemDto = new OrderItemDTO(item);
+            items.add(itemDto);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public ClientDTO getClient() {
+        return client;
+    }
+
+    public PaymentDTO getPayment() {
+        return payment;
+    }
+
+    public List<OrderItemDTO> getItems() {
+        return items;
+    }
+
+    public Double getTotal() {
+        double sum = 0.0;
+        for (OrderItemDTO item : items) {
+            sum = sum + item.getSubtotal();
+        }
+        return sum;
+    }
+}
