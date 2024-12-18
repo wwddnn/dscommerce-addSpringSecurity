dedfdc0ec4a27ca80590b592b06dc520efff28ea
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2024-12-17 23:10:35 -0300

    Fix validation

--- src/main/java/com/devsuperior/dscommerce/dto/ValidationErrorDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ValidationErrorDTO.java
@@ -18,6 +18,7 @@ public class ValidationErrorDTO extends CustomErrorDTO {
     }
 
     public void addError(String fieldName, String message) {
+        errors.removeIf(x -> x.getFieldName().equals(fieldName));
         errors.add(new FieldMessageDTO(fieldName, message));
     }
 }
